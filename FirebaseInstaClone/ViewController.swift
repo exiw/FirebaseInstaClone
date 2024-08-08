1e673c61c4a0e355de59307e8259bf7c254b87cb
exiw <51192517+exiw@users.noreply.github.com>
2024-08-08 12:47:38 +0300

    Initial Commit

---
+++ FirebaseInstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseInstaClone
+//
+//  Created by Emre Konukpay on 8.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
